72ef79e5c0799ad1d93ec2ffb99456f9a1250f53
geo_prepod_pc <romkagp@yandex.ru>
2025-04-23 21:33:01 +0300

    pre-release 3

--- data/telegram_text.py
+++ data/telegram_text.py
@@ -1,8 +1,8 @@
 import requests
-
+from data.config import TOKEN
 
 def telegram(chat_id, message):
-    bot_token = '7519668045:AAH643SvGHNjqK_ivjhBs7uL5gQc-teg4gg'
+    bot_token = TOKEN
     send_message_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
 
     payload = {
